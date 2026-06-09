0dc099fb91d4d31be2c456a7a93fef07bc8412c6
Abhinav Prasad E P <abhinav@Abhinavs-MacBook-Air.local>
2026-06-09 22:06:13 +0530

    Day 4: Added triangle and circle drawing

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
-#define ROWS 20
-#define COLS 40
+#include <stdlib.h>
+#define ROWS 25
+#define COLS 80
 
 char canvas[ROWS][COLS];
 
@@ -34,15 +34,46 @@ void drawRectangle(int r1, int c1, int r2, int c2)
 }
 void drawLine(int r1, int c1, int r2, int c2)
 {
-    if(r1 == r2)   // Horizontal line
+    int dx = c2 - c1;
+    int dy = r2 - r1;
+
+    int steps = abs(dx);
+
+    if(abs(dy) > steps)
+        steps = abs(dy);
+
+    for(int i = 0; i <= steps; i++)
     {
-        for(int j = c1; j <= c2; j++)
-            canvas[r1][j] = '*';
+        int r = r1 + (dy * i) / steps;
+        int c = c1 + (dx * i) / steps;
+
+        if(r >= 0 && r < ROWS && c >= 0 && c < COLS)
+            canvas[r][c] = '*';
     }
-    else if(c1 == c2)   // Vertical line
+}
+void drawTriangle(int r1, int c1,
+                  int r2, int c2,
+                  int r3, int c3)
+{
+    drawLine(r1, c1, r2, c2);
+    drawLine(r2, c2, r3, c3);
+    drawLine(r3, c3, r1, c1);
+}
+void drawCircle(int centerRow, int centerCol, int radius)
+{
+    for(int i = 0; i < ROWS; i++)
     {
-        for(int i = r1; i <= r2; i++)
-            canvas[i][c1] = '*';
+        for(int j = 0; j < COLS; j++)
+        {
+            int d = (i - centerRow) * (i - centerRow)
+                  + (j - centerCol) * (j - centerCol);
+
+            if(d >= radius * radius - radius &&
+               d <= radius * radius + radius)
+            {
+                canvas[i][j] = '*';
+            }
+        }
     }
 }
 void menu()
@@ -51,6 +82,8 @@ void menu()
     printf("1. Display Canvas\n");
     printf("2. Draw Rectangle\n");
     printf("3. Draw Line\n");
+    printf("4. Draw Triangle\n");
+    printf("5. Draw Circle\n");
     printf("0. Exit\n");
 }
 
@@ -82,17 +115,42 @@ void runMenu()
                 break;
             }
             case 3:
-{
-    int r1, c1, r2, c2;
+            {
+            int r1, c1, r2, c2;
 
-    printf("Line (row1 col1 row2 col2): ");
-    scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+             printf("Line (row1 col1 row2 col2): ");
+             scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
 
-    drawLine(r1, c1, r2, c2);
+            drawLine(r1, c1, r2, c2);
 
-    printf("Line drawn!\n");
-    break;
-}
+            printf("Line drawn!\n");
+            break;
+            }
+            case 4:
+            {
+             int r1, c1, r2, c2, r3, c3;
+
+            printf("Enter row1 col1 row2 col2 row3 col3: ");
+            scanf("%d %d %d %d %d %d",
+            &r1, &c1, &r2, &c2, &r3, &c3);
+
+            drawTriangle(r1, c1, r2, c2, r3, c3);
+
+             printf("Triangle drawn!\n");
+             break;
+             }
+             case 5:
+           {
+            int row, col, radius;
+
+             printf("Enter center row center col radius: ");
+            scanf("%d %d %d", &row, &col, &radius);
+
+            drawCircle(row, col, radius);
+
+            printf("Circle drawn!\n");
+            break;
+           }
 
             case 0:
                 printf("Exiting program...\n");
